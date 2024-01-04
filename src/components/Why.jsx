93b7ef88e3abbcae2bd68055363fb2df5b2b3e93
zablon <zablonoigo51@gmail.com>
2024-01-04 14:44:11 +0300

    changing text height and width on smaller screen

--- src/components/Why.jsx
+++ src/components/Why.jsx
@@ -10,31 +10,39 @@ function Why() {
     }
   return (
  <>
- <div className="min-h-[400px] md:px-20 bg-right-top px-5 py-10 bg-gray-100 relative" style={bgImage}>
-    <div className="bg-black w-full absolute right-0 top-0 h-[400px] bg-opacity-60 md:px-20 px-5 py-20">
+ <div className="md:min-h-[400px] min-h-screen md:px-20 bg-right-top px-5 py-10 bg-gray-100 relative" style={bgImage}>
+    <div className="bg-black w-full absolute right-0 top-0 md:h-[400px] h-screen bg-opacity-60 md:px-20 px-5 py-20">
         <div className="flex items-center justify-center gap-10 flex-col-reverse md:flex-row">
         <div className="bg-white rounded-xl shadow-2xl w-full md:w-1/2">
              <ul className="space-y-6 p-10">
                 <li className="inline-flex items-center gap-1">
                 <CiCircleCheck  className='text-2xl text-rose-500'/>
-                Move In/Outs & Deep Cleaning services only $59
+                <span className="text-sm md:text-lg">
+                    Move In/Outs & Deep Cleaning services only $59
+                    </span>
                 </li>
                 <li className="inline-flex items-center gap-1">
                 <CiCircleCheck  className='text-2xl text-rose-500'/>
+                <span className="text-sm md:text-lg">
                 Recurring discounts (monthly 5% off)
+                </span>
                 </li>               
                  <li className="inline-flex items-center gap-1">
                 <CiCircleCheck  className='text-2xl text-rose-500'/>
+                <span className="text-sm md:text-lg">
                 Simple flat rate pricing for you & your family!
+                </span>
                 </li>               
                  <li className="inline-flex items-center gap-1">
                 <CiCircleCheck  className='text-2xl text-rose-500'/>
+                <span className="text-sm md:text-lg">
                 Easy online booking
+                </span>
                 </li>
              </ul>
         </div>
         <div className="w-full md:w-1/2" >
-         <h2 className="text-5xl font-bold text-yellow-400 capitalize mb-6">
+         <h2 className="md:text-5xl text-2xl  font-bold text-yellow-400 capitalize mb-6">
          Get Your Place Cleaned and Healthy
          </h2>
          <p className="text-xl text-white font-light">
