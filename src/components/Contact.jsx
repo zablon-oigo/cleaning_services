0c5e6096824e33422ec06891446958312fcec8c0
zablon <zablonoigo51@gmail.com>
2024-01-21 16:35:24 +0300

    feat: Added transition

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,7 +11,7 @@ function Contact() {
   <div className="min-h-[600px] bg-gradient-to-br from-yellow-300 to-yellow-600 md:px-20 md:py-20 px-5 py-5">
     <h2 className="text-5xl text-center font-bold text-white">Contact Us</h2>
             <div className="flex flex-col-reverse md:flex-row items-center justify-between md:gap-10 gap-4">
-            <div className="mt-10 w-full md:w-1/2" data-aos='fade-right'>
+            <div className="mt-10 w-full md:w-1/2" data-aos='fade-right' data-aos-duration='3000' data-easing='linear'>
         <form action="" className="">
             <label htmlFor="" className="block text-xl mb-2 text-white">Name</label>
             <input type="text" className="px-6 py-3 w-full rounded-xl " />
@@ -22,7 +22,7 @@ function Contact() {
             <button className="mt-2 px-6 py-3 rounded-xl w-full bg-teal-500 text-white text-xl">Submit</button>
         </form>
     </div>
-    <div className="mt-10 w-full md:w-1/2" data-aos='fade-left'>
+    <div className="mt-10 w-full md:w-1/2" data-aos='fade-left' data-aos-duration='3000' data-easing='linear'>
         <img src={img} alt="call" className='rounded shadow-2xl'/>
     </div>
             </div>
