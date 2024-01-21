c72e92052744d3dc446c59d8e607e829c87b1803
zablon <zablonoigo51@gmail.com>
2024-01-21 16:39:49 +0300

    Feat: Scroll Effect

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -11,7 +11,7 @@ function Services() {
     < >
         <div className="min-h-screen md:px-20 px-5 py-10 bg-gray-100">
             <h2 className="md:text-5xl text-3xl font-bold capitalize text-center">our <span className="text-yellow-300">services</span></h2>
-            <div className="mt-10 grid md:grid-cols-3 gap-6 grid-cols-1" data-aos="fade-left">
+            <div className="mt-10 grid md:grid-cols-3 gap-6 grid-cols-1" data-aos="fade-left" data-aos-duration="3000" data-easing="linear">
             <ServicesCard/>
             </div>
         </div>
